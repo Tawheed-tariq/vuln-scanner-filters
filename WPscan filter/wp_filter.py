160ce8f79ade9a41e2e19ad45560a9746f964eec
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-02-23 13:48:30 +0530

    initialized wp filter

--- WPscan filter/wp_filter.py
+++ WPscan filter/wp_filter.py
@@ -10,3 +10,26 @@ filePath = os.path.join(current_directory, 'wpscan.txt')
 wp_output = read_file(filePath)
 
 
+def parse_wp_results(wp_output):
+    robots_txt_pattern = re.compile(r'robots.txt found: (.*)')
+    xml_rpc_pattern = re.compile(r'XML-RPC seems to be enabled: (.*)')
+    wordpress_readme_pattern = re.compile(r'WordPress readme found: (.*)')
+    wordpress_version_pattern = re.compile(r'WordPress version (.+?)\s')
+
+    robots_match = robots_txt_pattern.search(wp_output)
+    xml_match = xml_rpc_pattern.search(wp_output)
+    wordpress_readme = wordpress_readme_pattern.search(wp_output)
+    wordpress_version = wordpress_version_pattern.search(wp_output)
+
+    matches = {
+        'robots.txt' : robots_match.group(1),
+        'xml-RPC' : xml_match.group(1),
+        'wordpress readme' : wordpress_readme.group(1),
+        'wordpress version' : wordpress_version.group(1)
+    }
+
+    return matches
+
+
+result = parse_wp_results(wp_output=wp_output)
+print(result)

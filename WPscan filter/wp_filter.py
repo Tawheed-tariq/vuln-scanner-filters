d6bee91af2b5b8b5dc03a1658cf10000c1cf6c6a
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-02-24 13:00:46 +0530

    commit

--- WPscan filter/wp_filter.py
+++ WPscan filter/wp_filter.py
@@ -26,76 +26,92 @@ def parse_wp_results(wp_output):
 
     return matches
 
+#finds vulnerabilities of each plugin
+def plugin_vulnerabilities(vulnerabilities):
+    pattern = r'\[!\](.*?)References'
+    vuln_arr = re.findall(pattern , vulnerabilities.group(0).strip(), re.DOTALL)
+    num_of_vuln = len(vuln_arr)
+
+    #find title and version of each vulnerability
+    vulns = []
+    for vuln in vuln_arr:
+        title_pattern = re.compile(r'Title: (.*)')
+        fixed_pattern = re.compile(r'Fixed in: (.*)')
+
+        title = title_pattern.search(vuln)
+        fixed = fixed_pattern.search(vuln)
+
+        data = {
+            'title' : title.group(1),
+            'Version' : fixed.group(1)
+        }
+        vulns.append(data)
+
+
+    plugin_vuln_data = {
+        'number' : num_of_vuln,
+        'vulns' : vulns
+    }
+    return plugin_vuln_data
+
 #scrapes the number of vulnerabilities and title, version of each vulnerability and returns an array (output data)
 def find_vulnerabilities(wp_output):
     try:
         plugins_pattern = r'\[i\] Plugin\(s\) Identified:\n(.*?)(?:Interesting Finding\(s\):|\[i\])'
-
         plugins = re.search(plugins_pattern, wp_output, re.DOTALL)
-        plugin_output = plugins.group(1).strip() #plugins part of the wp_output
-        plugin_arr = re.findall(r'\[\+\]\s(.*?)\n\n',plugin_output , re.DOTALL) #scrapes all the plugins present in plugin_output
 
-        #find vulnerabilities of each plugin
-        output_data = []
-        for plugin in plugin_arr:
-            vuln_pattern = r'vulnerabilit.* identified:(.*)'
-            vulnerabilities = re.search(vuln_pattern,plugin, re.DOTALL)
+        #if plugins are present then it will execute this code
+        if(plugins):
+            plugin_output = plugins.group(1).strip() #plugins part of the wp_output
+            plugin_arr = re.findall(r'\[\+\]\s(.*?)\n\n',plugin_output , re.DOTALL) #scrapes all the plugins present in plugin_output
 
+            #find vulnerabilities of each plugin
+            output_data = []
+            for plugin in plugin_arr:
+                vuln_pattern = r'vulnerabilit.* identified:(.*)'
+                vulnerabilities = re.search(vuln_pattern,plugin, re.DOTALL)
 
-            # if the line "vulnerabilities identified" is present in plugin the this will execute
-            if(vulnerabilities):
-                pattern = r'\[!\](.*?)References'
-                vuln_arr = re.findall(pattern , vulnerabilities.group(0).strip(), re.DOTALL)
-                num_of_vuln = len(vuln_arr)
+                # if the line "vulnerabilities identified" is present in plugin the this will execute
+                if(vulnerabilities):
+                    plugin_vuln_data = plugin_vulnerabilities(vulnerabilities)
+                    output_data.append(plugin_vuln_data)
 
-                #find title and version of each vulnerability
-                vulns = []
-                for vuln in vuln_arr:
-                    title_pattern = re.compile(r'Title: (.*)')
-                    fixed_pattern = re.compile(r'Fixed in: (.*)')
-
-                    title = title_pattern.search(vuln)
-                    fixed = fixed_pattern.search(vuln)
-
-                    data = {
-                        'title' : title.group(1),
-                        'Version' : fixed.group(1)
-                    }
-                    vulns.append(data)
-
-
-                plugin_vuln_data = {
-                    'number' : num_of_vuln,
-                    'vulns' : vulns
-                }
-                output_data.append(plugin_vuln_data)
-        return output_data
+            return output_data if len(output_data) > 0 else None
     except:
         print('error')
 
 
 
 def find_users(wp_output):
-    users_pattern = r'\[i\] User\(s\) Identified:\n(.*?)\[\+\] WPScan DB API'
+    users_pattern = r'\[i\] User\(s\) Identified:\n(.*?)(?:\[\+\] WPScan DB API|\[\+\] Finished|$)'
     users = re.search(users_pattern, wp_output , re.DOTALL)
     users_output = users.group(1).strip()
+
     pattern = r'\[\+\](.*)'
     users_arr = re.findall(pattern, users_output)
+    
     return users_arr
 
 
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-filePath = os.path.join(current_directory, 'wpscan3.txt')
+filePath = os.path.join(current_directory, 'wpscan.txt')
 wp_output = read_file(filePath)
 
 
 result = parse_wp_results(wp_output)
 print(result)
 print('\n')
+
+
 vulnerabilities = find_vulnerabilities(wp_output)
-for vuln in vulnerabilities:
-    print(vuln)
-    print('\n')
+if(vulnerabilities):
+    for vuln in vulnerabilities:
+        print(vuln)
+        print('\n')
+else:
+    print(f"vulnerabilities are : {vulnerabilities}")
+
+
 users = find_users(wp_output)
 print(users)
